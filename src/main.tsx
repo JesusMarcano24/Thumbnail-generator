031d5d84d4bedde4806cc58917fb70b10e2034e5
Jesus Marcano <jesusmarcanu24@gmail.com>
2023-10-07 13:15:29 -0500

    adding tanstack provider and devtools

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ThemeConfig } from './Config/Theme.config.tsx'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeConfig>
-      <App />
-    </ThemeConfig>
+    <QueryClientProvider client={queryClient}>
+        <ThemeConfig>
+          <App />
+        </ThemeConfig>
+        <ReactQueryDevtools/>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
