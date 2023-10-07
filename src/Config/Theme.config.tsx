11eefa648c33509cc54e7692137a113085eccffa
Jesus Marcano <jesusmarcanu24@gmail.com>
2023-10-07 12:45:16 -0500

    adding some general styles with MUI

---
+++ src/Config/Theme.config.tsx
@@ -0,0 +1,29 @@
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import React from "react";
+
+type ThemeProp = {
+  children: JSX.Element;
+};
+
+export enum themePalette {
+  BG = "#12181b",
+}
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: themePalette.BG,
+      paper: themePalette.BG,
+    },
+  },
+});
+
+export const ThemeConfig: React.FC<ThemeProp> = ({ children }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+};
