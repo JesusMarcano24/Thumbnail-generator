534a8596fee0d9678c2b02162503d35c0832189c
Jesus Marcano <jesusmarcanu24@gmail.com>
2023-10-10 19:30:10 -0500

    adding some validations in the resize view

--- src/Pages/Resize.tsx
+++ src/Pages/Resize.tsx
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import Resizer from "react-image-file-resizer";
 import Loader from "../Common/Loader";
+import Box from '@mui/material/Box';
+import CircularProgress from '@mui/material/CircularProgress';
+import { green } from '@mui/material/colors';
+import Button from '@mui/material/Button';
+import Fab from '@mui/material/Fab';
+import CheckIcon from '@mui/icons-material/Check';
+import DownloadIcon from '@mui/icons-material/Download';
 
 function Resize() {
   const [width, setWidth] = useState<number>(0);
@@ -8,10 +15,38 @@ function Resize() {
   const [select, setSelect] = useState<File | null>(null);
   const [format, setFormat] = useState<string>("");
 
-  console.log(format)
+  const [loading, setLoading] = React.useState(false);
+  const [success, setSuccess] = React.useState(false);
+  const timer = React.useRef<number>();
 
-  //Preview
-  const [previewImage, setPreviewImage] = useState<string | null>(null);
+    //Preview
+    const [previewImage, setPreviewImage] = useState<string | null>(null);
+
+  const buttonSx = {
+    ...(success && {
+      bgcolor: green[500],
+      '&:hover': {
+        bgcolor: green[700],
+      },
+    }),
+  };
+
+  React.useEffect(() => {
+    return () => {
+      clearTimeout(timer.current);
+    };
+  }, []);
+
+  const handleButtonClick = () => {
+    if (!loading) {
+      setSuccess(false);
+      setLoading(true);
+      timer.current = window.setTimeout(() => {
+        setSuccess(true);
+        setLoading(false);
+      }, 2000);
+    }
+  };
 
   const fileChangedHandler = () => {
     if (select) {
@@ -95,7 +130,58 @@ function Resize() {
           <img src={previewImage} alt="Preview" />
       )}
 
-    <button onClick={downloadImage}>Download</button>
+    {!!previewImage && ( 
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Box sx={{ m: 1, position: 'relative' }}>
+          <Fab
+            aria-label="save"
+            color="primary"
+            sx={buttonSx}
+            onClick={handleButtonClick}
+          >
+            {success ? <CheckIcon /> : <DownloadIcon />}
+          </Fab>
+          {loading && (
+            <CircularProgress
+              size={68}
+              sx={{
+                color: green[500],
+                position: 'absolute',
+                top: -6,
+                left: -6,
+                zIndex: 1,
+              }}
+            />
+          )}
+        </Box>
+        <Box sx={{ m: 1, position: 'relative' }}>
+          <Button
+              variant="contained"
+              sx={buttonSx}
+              disabled={loading}
+              onClick={() => {
+                handleButtonClick();
+                downloadImage();
+              }}
+            >
+              Download Image
+            </Button>
+            {loading && (
+              <CircularProgress
+                size={24}
+                sx={{
+                  color: green[500],
+                  position: 'absolute',
+                  top: '50%',
+                  left: '50%',
+                  marginTop: '-12px',
+                  marginLeft: '-12px',
+                }}
+              />
+            )}
+        </Box>
+      </Box>
+    )}
     </div>
   );
 }
